e39e54c82c39b4a7f1db1c4d82bb3e9b3d4493a4
DImonchIKbg <112264354+DImonchIKbg@users.noreply.github.com>
2022-08-27 21:03:36 +0600

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,14 @@
+local Window = Library.CreateLib("DM studio", "RJTheme3")
+
+local Tab = Window:NewTab("speed")
+
+local Section = Tab:NewSection("Walk Speed")
+
+
+Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
+	print("You just clicked the bind")
+end)
